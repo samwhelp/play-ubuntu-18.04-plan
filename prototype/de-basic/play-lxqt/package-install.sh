cee329862d30b4ada0cbca1a54b52a4c6614d9be
samwhelp <samwhelp@users.noreply.github.com>
2018-03-13 12:17:52 +0800

    comment in package-list.txt allowed

--- prototype/de-basic/play-lxqt/package-install.sh
+++ prototype/de-basic/play-lxqt/package-install.sh
@@ -2,10 +2,45 @@
 
 set -e
 
+
+### Head: util_package #########################################################
+#
+util_package_find_list () {
+	local file_path="$1"
+	cat $file_path  | while IFS='' read -r line; do
+		trim_line=$(echo $line) # trim
+
+		## ignore leading #
+		if [ "${trim_line:0:1}" == '#' ]; then
+			continue;
+		fi
+
+		## ignore empty line
+		if [[ -z "$trim_line" ]]; then
+			continue;
+		fi
+
+		echo "$line"
+	done
+}
+
+main_package_find_list () {
+	util_package_find_list "package-list.txt"
+}
+
+main_package_find_list_raw () {
+	cat "package-list.txt"
+}
+#
+### Tail: util_package #########################################################
+
+
 ### Head: main #################################################################
 #
 main_package_install () {
-	sudo apt-get install $(cat package-list.txt)
+	# sudo apt-get install $(cat package-list.txt)
+	# sudo apt-get install $(main_package_find_list_raw)
+	sudo apt-get install $(main_package_find_list)
 }
 
 main_package_install
