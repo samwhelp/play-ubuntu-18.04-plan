274fa807a4a82f67dd57abcec502dcb3ba774aed
samwhelp <samwhelp@users.noreply.github.com>
2018-03-17 22:08:35 +0800

    fix

--- prototype/env-basic/apt-sources-list/tw-install.sh
+++ prototype/env-basic/apt-sources-list/tw-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
+echo "sudo cp ./config/tw/sources.list /etc/apt/sources.list"
 sudo cp ./config/tw/sources.list /etc/apt/sources.list
 
-
-#sudo apt-get update
+echo "sudo apt-get update"
+sudo apt-get update
 # ls /var/lib/apt/lists/ -1
 # ls /var/lib/apt/lists/ -l
--- prototype/env-basic/apt-sources-list/us-install.sh
+++ prototype/env-basic/apt-sources-list/us-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
+echo "sudo cp ./config/us/sources.list /etc/apt/sources.list"
 sudo cp ./config/us/sources.list /etc/apt/sources.list
 
-
-#sudo apt-get update
+echo "sudo apt-get update"
+sudo apt-get update
 # ls /var/lib/apt/lists/ -1
 # ls /var/lib/apt/lists/ -l
