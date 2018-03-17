8c563a8b7236c3ca4ba6c0cb74d10f7cec88cca3
samwhelp <samwhelp@users.noreply.github.com>
2018-03-17 19:54:41 +0800

    add

--- prototype/env-basic/locale/locale-gen.sh
+++ prototype/env-basic/locale/locale-gen.sh
@@ -12,3 +12,11 @@ sudo locale-gen zh_TW zh_TW.UTF-8 zh_CN.UTF-8 en_US.UTF-8
 # /etc/locale.gen
 # /usr/lib/locale/locale-archive
 # strings /usr/lib/locale/locale-archive| grep zh_TW
+
+
+
+# sudo dpkg-reconfigure locales
+# /var/lib/dpkg/info/locales.config
+# /var/lib/dpkg/info/locales.postinst
+# /var/lib/dpkg/info/locales.templates
+# /var/lib/dpkg/info/locales.prerm
