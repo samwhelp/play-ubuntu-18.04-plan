56e3eae58c798dffa6b1e8c2a7d03abf3c0b7df9
samwhelp <samwhelp@users.noreply.github.com>
2018-03-17 23:33:47 +0800

    fix

--- prototype/env-basic/locale/en_us-install.sh
+++ prototype/env-basic/locale/en_us-install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 
+echo "sudo cp ./config/en_US/locale /etc/default/locale"
 sudo cp ./config/en_US/locale /etc/default/locale
+
+echo
+
+echo "cat /etc/default/locale"
+cat /etc/default/locale
--- prototype/env-basic/locale/zh_tw-install.sh
+++ prototype/env-basic/locale/zh_tw-install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 
+echo "sudo cp ./config/zh_TW/locale /etc/default/locale"
 sudo cp ./config/zh_TW/locale /etc/default/locale
+
+echo
+
+echo "cat /etc/default/locale"
+cat /etc/default/locale
