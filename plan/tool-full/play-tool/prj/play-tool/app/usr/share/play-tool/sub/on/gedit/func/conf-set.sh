2758163e9e3712cdebc002269e0797b92b8b9cd9
samwhelp <samwhelp@users.noreply.github.com>
2018-03-22 08:14:49 +0800

    gedit

---
+++ plan/tool-full/play-tool/prj/play-tool/app/usr/share/play-tool/sub/on/gedit/func/conf-set.sh
@@ -0,0 +1,42 @@
+
+
+### Head: main #################################################################
+#
+gedit_conf_set () {
+	echo
+	echo "### Head: gedit_conf_set ########################################################"
+	echo "#"
+
+	gedit_conf_set_config
+
+	echo "#"
+	echo "### Tail: gedit_conf_set ########################################################"
+	echo
+}
+#
+### Tail: main #################################################################
+
+
+### Head: gedit ################################################################
+#
+gedit_conf_set_config () {
+	## gsettings list-recursively | grep edit
+
+	#echo "gsettings set org.gnome.gedit.state.window side-panel-active-page 'GeditWindowDocumentsPanel'"
+	#gsettings set org.gnome.gedit.state.window side-panel-active-page 'GeditWindowDocumentsPanel'
+	echo "gsettings set org.gnome.gedit.state.window side-panel-active-page 'GeditFileBrowserPanel'"
+	gsettings set org.gnome.gedit.state.window side-panel-active-page 'GeditFileBrowserPanel'
+	#gsettings reset org.gnome.gedit.state.window side-panel-active-page
+	#gsettings get org.gnome.gedit.state.window side-panel-active-page
+
+
+	## gsettings list-recursively | grep edit | grep encodings
+
+
+	echo "gsettings set org.gnome.gedit.preferences.encodings candidate-encodings \"['UTF-8', 'BIG5', 'BIG5-HKSCS', 'EUC-TW', 'GB18030', 'GB2312', 'GBK', 'CURRENT', 'ISO-8859-15', 'UTF-16']\""
+	gsettings set org.gnome.gedit.preferences.encodings candidate-encodings "['UTF-8', 'BIG5', 'BIG5-HKSCS', 'EUC-TW', 'GB18030', 'GB2312', 'GBK', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
+	#gsettings reset org.gnome.gedit.preferences.encodings candidate-encodings
+	#gsettings get org.gnome.gedit.preferences.encodings candidate-encodings
+}
+#
+### Tail: gedit ################################################################
