e245052ffd6444f85631496cfeb5cd619bc91e53
samwhelp <samwhelp@users.noreply.github.com>
2018-03-25 00:07:52 +0800

    chrome

---
+++ plan/de-full/play-lxqt/prj/play-lxqt/app/usr/share/play-lxqt/sub/on/chrome/func/app_remove.sh
@@ -0,0 +1,27 @@
+
+
+### Head: main #################################################################
+#
+chrome_app_remove () {
+	echo
+	echo "### Head: chrome_app_remove ######################################################"
+	echo "#"
+
+	chrome_app_remove_apt_remove
+
+	echo "#"
+	echo "### Tail: chrome_app_remove ######################################################"
+	echo
+}
+#
+### Tail: main #################################################################
+
+
+### Head: chrome #################################################################
+#
+chrome_app_remove_apt_remove () {
+	echo "sudo apt-get remove google-chrome-stable"
+	sudo apt-get remove google-chrome-stable
+}
+#
+### Tail: main #################################################################
