e4d94777cd9766cdfdb3fcf8a6849352be7b395e
samwhelp <samwhelp@users.noreply.github.com>
2018-10-29 13:09:30 +0800

    fix play-im install-fcitx-chewing

--- plan/im-full/play-im/prj/play-im/app/usr/share/play-im/sub/on/fcitx_chewing/func/conf_reset.sh
+++ plan/im-full/play-im/prj/play-im/app/usr/share/play-im/sub/on/fcitx_chewing/func/conf_reset.sh
@@ -30,6 +30,21 @@ fcitx_chewing_conf_reset_dir_create () {
 }
 
 fcitx_chewing_conf_reset_config () {
+	if fcitx_chewing_conf_reset_is_fcitx_running; then
+		echo
+		echo 'fcitx is running'
+		fcitx_chewing_conf_reset_stop_fcitx
+		fcitx_chewing_conf_reset_config_profile
+		fcitx_chewing_conf_reset_start_fcitx
+	else
+		echo
+		echo 'fcitx not running'
+		fcitx_chewing_conf_reset_config_profile
+		fcitx_chewing_conf_reset_start_fcitx
+	fi
+}
+
+fcitx_chewing_conf_reset_config_profile () {
 
 	echo
 	echo "cp $THE_SUB_ON_DIR_PATH/fcitx_chewing/conf/reset/profile $HOME/.config/fcitx/profile"
@@ -44,6 +59,44 @@ fcitx_chewing_conf_reset_im_config () {
 	im-config -n REMOVE
 
 	echo
+}
+fcitx_chewing_conf_reset_im_config () {
+
+	echo
+
+	echo "im-config -n fcitx"
+	im-config -n fcitx
+
+	echo "cat ~/.xinputrc"
+	cat ~/.xinputrc
+
+	echo
+
+}
+
+fcitx_chewing_conf_reset_stop_fcitx () {
+	echo
+	echo "Stop fcitx"
+
+	#qdbus org.fcitx.Fcitx /inputmethod org.fcitx.Fcitx.InputMethod.Exit
+
+	killall -9 fcitx
+
+}
+
+fcitx_chewing_conf_reset_start_fcitx () {
+	echo
+	echo "Start fcitx"
+	fcitx -r
+}
+
+fcitx_chewing_conf_reset_is_fcitx_running () {
+	fcitx-remote &> /dev/null
+	if [ "$?" = "0" ]; then
+		return 0;
+	else
+		return 1;
+	fi
 }
 #
 ### Tail: fcitx_chewing ################################################################
--- plan/im-full/play-im/prj/play-im/app/usr/share/play-im/sub/on/fcitx_chewing/func/conf_set.sh
+++ plan/im-full/play-im/prj/play-im/app/usr/share/play-im/sub/on/fcitx_chewing/func/conf_set.sh
@@ -30,6 +30,21 @@ fcitx_chewing_conf_set_dir_create () {
 }
 
 fcitx_chewing_conf_set_config () {
+	if fcitx_chewing_conf_set_is_fcitx_running; then
+		echo
+		echo 'fcitx is running'
+		fcitx_chewing_conf_set_stop_fcitx
+		fcitx_chewing_conf_set_config_profile
+		fcitx_chewing_conf_set_start_fcitx
+	else
+		echo
+		echo 'fcitx not running'
+		fcitx_chewing_conf_set_config_profile
+		fcitx_chewing_conf_set_start_fcitx
+	fi
+}
+
+fcitx_chewing_conf_set_config_profile () {
 
 	echo
 	echo "cp $THE_SUB_ON_DIR_PATH/fcitx_chewing/conf/set/profile $HOME/.config/fcitx/profile"
@@ -49,6 +64,31 @@ fcitx_chewing_conf_set_im_config () {
 
 	echo
 
+}
+
+fcitx_chewing_conf_set_stop_fcitx () {
+	echo
+	echo "Stop fcitx"
+
+	#qdbus org.fcitx.Fcitx /inputmethod org.fcitx.Fcitx.InputMethod.Exit
+
+	killall -9 fcitx
+
+}
+
+fcitx_chewing_conf_set_start_fcitx () {
+	echo
+	echo "Start fcitx"
+	fcitx -r
+}
+
+fcitx_chewing_conf_set_is_fcitx_running () {
+	fcitx-remote &> /dev/null
+	if [ "$?" = "0" ]; then
+		return 0;
+	else
+		return 1;
+	fi
 }
 #
 ### Tail: fcitx_chewing ################################################################
