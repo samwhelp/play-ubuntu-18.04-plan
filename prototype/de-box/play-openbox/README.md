d58f85dc6d02ab72abedfd0c154314486077fd81
samwhelp <samwhelp@users.noreply.github.com>
2019-12-07 00:02:05 +0800

    play-openbox

---
+++ prototype/de-box/play-openbox/README.md
@@ -0,0 +1,62 @@
+
+# Prototype play-openbox
+
+## Usage
+
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
+
+
+## Keyboard shortcuts
+
+Please refer to
+
+* $ less ~/.config/openbox/rc.xml
+
+
+## Link
+
+* http://openbox.org/
+
+
+## Theme
+
+* https://www.box-look.org/
+* https://www.box-look.org/browse/cat/140/order/latest/
+* https://www.box-look.org/browse/cat/140/ord/rating/
---
+++ prototype/de-box/play-openbox/config-install.sh
@@ -0,0 +1,212 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: openbox
+##
+openbox_config_install () {
+	mkdir -p "$HOME/.config/openbox"
+	echo "mkdir -p $HOME/.config/openbox"
+
+	cp "./config/openbox/autostart" "$HOME/.config/openbox/autostart"
+	echo "cp ./config/openbox/autostart $HOME/.config/openbox/autostart"
+
+	cp "./config/openbox/rc.xml" "$HOME/.config/openbox/rc.xml"
+	echo "cp ./config/openbox/rc.xml $HOME/.config/openbox/rc.xml"
+
+}
+##
+### Tail: openbox
+################################################################################
+
+
+################################################################################
+### Head: tint2
+##
+tint2_config_install () {
+
+	mkdir -p "$HOME/.config/tint2"
+	echo "mkdir -p $HOME/.config/tint2"
+
+	cp "./config/tint2/tint2rc" "$HOME/.config/tint2/tint2rc"
+	echo "cp ./config/tint2/tint2rc $HOME/.config/tint2/tint2rc"
+
+}
+##
+### Tail: tint2
+################################################################################
+
+
+################################################################################
+### Head: volumeicon
+##
+volumeicon_config_install () {
+
+	mkdir -p "$HOME/.config/volumeicon"
+	echo "mkdir -p $HOME/.config/volumeicon"
+
+	cp "./config/volumeicon/volumeicon" "$HOME/.config/volumeicon/volumeicon"
+	echo "cp ./config/volumeicon/volumeicon $HOME/.config/volumeicon/volumeicon"
+
+}
+##
+### Tail: volumeicon
+################################################################################
+
+
+################################################################################
+### Head: compton
+##
+compton_config_install () {
+
+	## $ dpkg -L compton | grep conf
+	## /usr/share/doc/compton/examples/compton.sample.conf
+
+	## cp $(dpkg -L compton | grep conf) ~/.config/compton.conf
+
+	echo "cp /usr/share/doc/compton/examples/compton.sample.conf $HOME/.config/compton.conf"
+	cp "/usr/share/doc/compton/examples/compton.sample.conf" "$HOME/.config/compton.conf"
+
+}
+##
+### Tail: compton
+################################################################################
+
+
+################################################################################
+### Head: pcmanfm-qt
+##
+pcmanfm_qt_config_install () {
+
+	mkdir -p "$HOME/.config/pcmanfm-qt/default"
+	echo "mkdir -p $HOME/.config/pcmanfm-qt/default"
+
+	cp "./config/pcmanfm-qt/default/settings.conf" "$HOME/.config/pcmanfm-qt/default/settings.conf"
+	echo "cp ./config/pcmanfm-qt/default/settings.conf $HOME/.config/pcmanfm-qt/default/settings.conf"
+
+}
+##
+### Tail: pcmanfm-qt
+################################################################################
+
+
+################################################################################
+### Head: rofi
+##
+rofi_config_install () {
+	mkdir -p "$HOME/.config/rofi"
+	echo "mkdir -p $HOME/.config/rofi"
+
+	cp "./config/rofi/config" "$HOME/.config/rofi/config"
+	echo "cp ./config/rofi/config $HOME/.config/rofi/config"
+}
+##
+### Tail: rofi
+################################################################################
+
+
+################################################################################
+### Head: sakura
+##
+sakura_config_install () {
+	mkdir -p "$HOME/.config/sakura"
+	echo "mkdir -p $HOME/.config/sakura"
+
+	cp "./config/sakura/sakura.conf" "$HOME/.config/sakura/sakura.conf"
+	echo "cp ./config/sakura/sakura.conf $HOME/.config/sakura/sakura.conf"
+}
+##
+### Tail: sakura
+################################################################################
+
+
+################################################################################
+### Head: fcitx
+##
+fcitx_config_install () {
+	mkdir -p "$HOME/.config/fcitx"
+	echo "mkdir -p $HOME/.config/fcitx"
+
+	cp "./config/fcitx/profile" "$HOME/.config/fcitx/profile"
+	echo "cp ./config/fcitx/profile $HOME/.config/fcitx/profile"
+
+	fcitx_config_install_im_config
+
+}
+
+fcitx_config_install_im_config () {
+	echo
+	im-config -n fcitx
+	echo "im-config -n fcitx"
+	echo "cat ~/.xinputrc"
+	cat ~/.xinputrc
+	echo
+}
+##
+### Tail: fcitx
+################################################################################
+
+
+################################################################################
+### Head: gtk3
+##
+gtk3_config_install () {
+	mkdir -p "$HOME/.config/gtk-3.0"
+	echo "mkdir -p $HOME/.config/gtk-3.0"
+
+	cp "./config/gtk3/settings.ini" "$HOME/.config/gtk-3.0/settings.ini"
+	echo "cp ./config/gtk3/settings.ini $HOME/.config/gtk-3.0/settings.ini"
+}
+##
+### Tail: gtk3
+################################################################################
+
+
+################################################################################
+### Head: gtk2
+##
+gtk2_config_install () {
+
+	cp "./config/gtk2/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+	echo "cp ./config/gtk2/.gtkrc-2.0 $HOME/.gtkrc-2.0"
+
+}
+##
+### Tail: gtk2
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_config_install () {
+
+	openbox_config_install
+
+	tint2_config_install
+
+	volumeicon_config_install
+
+	compton_config_install
+
+	pcmanfm_qt_config_install
+
+	rofi_config_install
+
+	sakura_config_install
+
+	fcitx_config_install
+
+	gtk3_config_install
+
+	gtk2_config_install
+
+}
+## start
+main_config_install
+
+##
+### Tail: main
+################################################################################
