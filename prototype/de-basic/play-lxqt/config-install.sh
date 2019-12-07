a2cdc7534cd33417f851230476ea43fc36a12af3
samwhelp <samwhelp@users.noreply.github.com>
2019-12-07 20:53:06 +0800

    lxqt panel rofi launch button

--- prototype/de-basic/play-lxqt/config-install.sh
+++ prototype/de-basic/play-lxqt/config-install.sh
@@ -74,11 +74,28 @@ pcmanfm_qt_config_install () {
 ### Head: rofi
 ##
 rofi_config_install () {
-	mkdir -p $HOME/.config/rofi
+	mkdir -p "$HOME/.config/rofi"
 	echo "mkdir -p $HOME/.config/rofi"
 
-	cp ./config/rofi/config $HOME/.config/rofi/config
+	cp "./config/rofi/config" "$HOME/.config/rofi/config"
 	echo "cp ./config/rofi/config $HOME/.config/rofi/config"
+
+	rofi_put_app_desktop_entry
+}
+
+rofi_put_app_desktop_entry () {
+	mkdir -p "$HOME/.local/share/applications"
+	echo "mkdir -p $HOME/.local/share/applications"
+
+	cp "./config/rofi/rofi-show-drun.desktop" "$HOME/.local/share/applications/rofi-show-drun.desktop"
+	echo "cp ./config/rofi/rofi-show-drun.desktop $HOME/.local/share/applications/rofi-show-drun.desktop"
+
+	cp "./config/rofi/rofi-show-run.desktop" "$HOME/.local/share/applications/rofi-show-run.desktop"
+	echo "cp ./config/rofi/rofi-show-run.desktop $HOME/.local/share/applications/rofi-show-run.desktop"
+
+	cp "./config/rofi/rofi-show-window.desktop" "$HOME/.local/share/applications/rofi-show-window.desktop"
+	echo "cp ./config/rofi/rofi-show-window.desktop $HOME/.local/share/applications/rofi-show-window.desktop"
+
 }
 ##
 ### Tail: rofi
