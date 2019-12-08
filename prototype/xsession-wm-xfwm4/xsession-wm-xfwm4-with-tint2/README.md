5b0fbcf1002fa0c70f01c0ed1bd0551246999feb
samwhelp <samwhelp@users.noreply.github.com>
2019-12-08 15:06:20 +0800

    xsession-wm-xfwm4-with-tint2 compton

---
+++ prototype/xsession-wm-xfwm4/xsession-wm-xfwm4-with-tint2/README.md
@@ -0,0 +1,51 @@
+
+# Prototype play-xfce
+
+
+## Usage
+
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
+
+
+## Keyboard shortcuts
+
+Please refer to
+
+* [~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml](config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml) ([xfwm4-settings](http://manpages.ubuntu.com/manpages/bionic/en/man1/xfwm4-settings.1.html)) ([xfce4-keyboard-settings](http://manpages.ubuntu.com/manpages/bionic/en/man1/xfce4-keyboard-settings.1.html))
---
+++ prototype/xsession-wm-xfwm4/xsession-wm-xfwm4-with-tint2/config/xsession/xfwm4-session.sh
@@ -0,0 +1,170 @@
+#!/bin/sh
+
+
+################################################################################
+### Head: Manual
+##
+
+#
+
+##
+### Tail: Manual
+################################################################################
+
+
+################################################################################
+### Head: Network
+##
+
+## network manager applet
+#sleep 1 &&
+nm-applet &
+
+##
+### Tail: Network
+################################################################################
+
+
+################################################################################
+### Head: Mixer
+##
+
+## sound mixer
+volumeicon &
+
+##
+### Tail: Mixer
+################################################################################
+
+
+################################################################################
+### Head: Desktop
+##
+
+## Use [pcmanfm-qt] for desktop
+## /etc/xdg/autostart/lxqt-desktop.desktop
+## grep '^Exec=' /etc/xdg/autostart/lxqt-desktop.desktop
+#pcmanfm-qt --desktop --profile=lxqt &
+
+## Use [xfdesktop] for desktop
+#xfdesktop &
+
+## Just Wallpaper
+
+#THE_WALLPAPER_FILE_PATH="/usr/share/backgrounds/Manhattan_Sunset_by_Giacomo_Ferroni.jpg"
+THE_WALLPAPER_FILE_PATH="/usr/share/backgrounds/Spices_in_Athens_by_Makis_Chourdakis.jpg"
+
+feh --bg-scale "$THE_WALLPAPER_FILE_PATH" &
+
+##
+### Tail: Desktop
+################################################################################
+
+
+################################################################################
+### Head: Panel
+##
+
+## stalonetray
+#stalonetray &
+
+## Panel
+
+## tint2
+tint2 -c "$HOME/.config/xfwm4-session/tint2rc" &
+
+## xfce4-panel
+#xfce4-panel &
+
+## fbpanel
+#fbpanel &
+
+## lxpanel
+#lxpanel &
+
+## lxqt-panel
+#lxqt-panel &
+
+## cairo-dock
+#cairo-dock &
+
+##
+### Tail: Panel
+################################################################################
+
+
+################################################################################
+### Head: Compton
+##
+
+## Please use [xfwm4 --compositor=off] if use compton.
+
+## compton
+#compton &
+#compton --config ~/.config/compton/compton.conf &
+compton --config ~/.config/xfwm4-session/compton.conf &
+
+##
+### Tail: Compton
+################################################################################
+
+
+################################################################################
+### Head: Shortcut
+##
+
+## global key shortcut
+lxqt-globalkeysd &
+
+
+##
+### Tail: Shortcut
+################################################################################
+
+
+################################################################################
+### Head: Terminal
+##
+
+## sakura
+#sakura -m &
+
+##
+### Tail: Terminal
+################################################################################
+
+
+################################################################################
+### Head: Window Manager
+##
+
+# $ man xfwm4
+
+# $ xfwm4 -h
+# Usage:
+#   xfwm4 [OPTION…] [ARGUMENTS...]
+#
+# Help Options:
+#   -h, --help                   Show help options
+#   --help-all                   Show all help options
+#   --help-gtk                   Show GTK+ Options
+#   --help-sm-client             Show session management options
+#
+# Application Options:
+#   --daemon                     Fork to the background
+#   --compositor=on|off|auto     Set the compositor mode
+#   --replace                    Replace the existing window manager
+#   -V, --version                Print version information and exit
+#   --display=DISPLAY            X display to use
+
+
+
+## xfwm4
+XFWM4_OPT_COMPOSITOR="--replace"
+XFWM4_OPT_COMPOSITOR="--compositor=off"
+
+exec xfwm4 $XFWM4_OPT_COMPOSITOR $XFWM4_OPT_COMPOSITOR
+
+##
+### Tail: Window Manager
+################################################################################
