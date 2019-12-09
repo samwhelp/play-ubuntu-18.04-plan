aaba3f82d64d0dfb5ba1d1cc5fa5b0135343c315
samwhelp <samwhelp@users.noreply.github.com>
2019-12-09 23:06:25 +0800

    fix

--- prototype/de-basic/play-i3/config-install.sh
+++ prototype/de-basic/play-i3/config-install.sh
@@ -7,10 +7,10 @@ set -e
 ### Head: i3
 ##
 i3_config_install () {
-	mkdir -p $HOME/.config/i3
+	mkdir -p "$HOME/.config/i3"
 	echo "mkdir -p $HOME/.config/i3"
 
-	cp ./config/i3/config $HOME/.config/i3/config
+	cp "./config/i3/config" "$HOME/.config/i3/config"
 	echo "cp ./config/i3/config $HOME/.config/i3/config"
 }
 ##
@@ -25,12 +25,13 @@ compton_config_install () {
 
 	## $ dpkg -L compton | grep conf
 	## /usr/share/doc/compton/examples/compton.sample.conf
-
 	## cp $(dpkg -L compton | grep conf) ~/.config/compton.conf
 
-	echo "cp /usr/share/doc/compton/examples/compton.sample.conf $HOME/.config/compton.conf"
-	cp "/usr/share/doc/compton/examples/compton.sample.conf" $HOME/.config/compton.conf
+	mkdir -p "$HOME/.config/i3/compton"
+	echo "mkdir -p $HOME/.config/i3/compton"
 
+	cp "/usr/share/doc/compton/examples/compton.sample.conf" "$HOME/.config/i3/compton/compton.conf"
+	echo "cp /usr/share/doc/compton/examples/compton.sample.conf $HOME/.config/i3/compton/compton.conf"
 }
 ##
 ### Tail: compton
@@ -45,7 +46,7 @@ pcmanfm_qt_config_install () {
 	mkdir -p "$HOME/.config/pcmanfm-qt/default"
 	echo "mkdir -p $HOME/.config/pcmanfm-qt/default"
 
-	cp ./config/pcmanfm-qt/default/settings.conf "$HOME/.config/pcmanfm-qt/default/settings.conf"
+	cp "./config/pcmanfm-qt/default/settings.conf" "$HOME/.config/pcmanfm-qt/default/settings.conf"
 	echo "cp ./config/pcmanfm-qt/default/settings.conf $HOME/.config/pcmanfm-qt/default/settings.conf"
 
 }
@@ -58,10 +59,10 @@ pcmanfm_qt_config_install () {
 ### Head: rofi
 ##
 rofi_config_install () {
-	mkdir -p $HOME/.config/rofi
+	mkdir -p "$HOME/.config/rofi"
 	echo "mkdir -p $HOME/.config/rofi"
 
-	cp ./config/rofi/config $HOME/.config/rofi/config
+	cp "./config/rofi/config" "$HOME/.config/rofi/config"
 	echo "cp ./config/rofi/config $HOME/.config/rofi/config"
 }
 ##
@@ -102,10 +103,10 @@ sakura_put_my_desktop_entry () {
 ### Head: fcitx
 ##
 fcitx_config_install () {
-	mkdir -p $HOME/.config/fcitx
+	mkdir -p "$HOME/.config/fcitx"
 	echo "mkdir -p $HOME/.config/fcitx"
 
-	cp ./config/fcitx/profile $HOME/.config/fcitx/profile
+	cp "./config/fcitx/profile" "$HOME/.config/fcitx/profile"
 	echo "cp ./config/fcitx/profile $HOME/.config/fcitx/profile"
 
 	fcitx_config_install_im_config
@@ -129,10 +130,10 @@ fcitx_config_install_im_config () {
 ### Head: gtk3
 ##
 gtk3_config_install () {
-	mkdir -p $HOME/.config/gtk-3.0
+	mkdir -p "$HOME/.config/gtk-3.0"
 	echo "mkdir -p $HOME/.config/gtk-3.0"
 
-	cp ./config/gtk3/settings.ini $HOME/.config/gtk-3.0/settings.ini
+	cp "./config/gtk3/settings.ini" "$HOME/.config/gtk-3.0/settings.ini"
 	echo "cp ./config/gtk3/settings.ini $HOME/.config/gtk-3.0/settings.ini"
 }
 ##
@@ -145,7 +146,7 @@ gtk3_config_install () {
 ##
 gtk2_config_install () {
 
-	cp ./config/gtk2/.gtkrc-2.0 $HOME/.gtkrc-2.0
+	cp "./config/gtk2/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
 	echo "cp ./config/gtk2/.gtkrc-2.0 $HOME/.gtkrc-2.0"
 
 }
