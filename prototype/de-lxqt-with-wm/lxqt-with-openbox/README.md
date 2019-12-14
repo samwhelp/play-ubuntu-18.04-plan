a04badfc1d94855238a5a474fe84d96e8f650971
samwhelp <samwhelp@users.noreply.github.com>
2019-12-14 15:03:54 +0800

    lxqt-with-openbox

---
+++ prototype/de-lxqt-with-wm/lxqt-with-openbox/README.md
@@ -0,0 +1,52 @@
+
+# Prototype play-lxqt
+
+
+## Usage
+
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
+
+
+## Keyboard shortcuts
+
+Please refer to
+
+* [~/.config/lxqt/globalkeyshortcuts.conf](config/lxqt/globalkeyshortcuts.conf) (lxqt-config-globalkeyshortcuts)
+* [~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml](config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml) ([xfwm4-settings](http://manpages.ubuntu.com/manpages/bionic/en/man1/xfwm4-settings.1.html))
---
+++ prototype/de-lxqt-with-wm/lxqt-with-openbox/config-install.sh
@@ -0,0 +1,247 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: lxqt
+##
+lxqt_config_install () {
+	mkdir -p $HOME/.config/lxqt
+	echo "mkdir -p $HOME/.config/lxqt"
+
+	cp ./config/lxqt/globalkeyshortcuts.conf $HOME/.config/lxqt/globalkeyshortcuts.conf
+	echo "cp ./config/lxqt/globalkeyshortcuts.conf $HOME/.config/lxqt/globalkeyshortcuts.conf"
+
+	cp ./config/lxqt/lxqt.conf $HOME/.config/lxqt/lxqt.conf
+	echo "cp ./config/lxqt/lxqt.conf $HOME/.config/lxqt/lxqt.conf"
+
+	cp ./config/lxqt/panel.conf $HOME/.config/lxqt/panel.conf
+	echo "cp ./config/lxqt/panel.conf $HOME/.config/lxqt/panel.conf"
+
+	cp ./config/lxqt/session.conf $HOME/.config/lxqt/session.conf
+	echo "cp ./config/lxqt/session.conf $HOME/.config/lxqt/session.conf"
+
+}
+##
+### Tail: lxqt
+################################################################################
+
+
+################################################################################
+### Head: openbox
+##
+openbox_config_install () {
+	mkdir -p "$HOME/.config/openbox"
+	echo "mkdir -p $HOME/.config/openbox"
+
+	cp "./config/openbox/lxqt-rc.xml" "$HOME/.config/openbox/lxqt-rc.xml"
+	echo "cp ./config/openbox/lxqt-rc.xml $HOME/.config/openbox/lxqt-rc.xml"
+
+	cp "./config/openbox/menu.xml" "$HOME/.config/openbox/menu.xml"
+	echo "cp ./config/openbox/menu.xml $HOME/.config/openbox/menu.xml"
+
+}
+##
+### Tail: openbox
+################################################################################
+
+
+################################################################################
+### Head: compton
+##
+compton_config_install () {
+
+	mkdir -p "$HOME/.config/lxqt/compton"
+	echo "mkdir -p $HOME/.config/lxqt/compton"
+
+
+	## $ dpkg -L compton | grep conf
+	## /usr/share/doc/compton/examples/compton.sample.conf
+	## cp $(dpkg -L compton | grep conf) ~/.config/compton.conf
+
+
+	##cp "/usr/share/doc/compton/examples/compton.sample.conf" "$HOME/.config/openbox/compton/compton.conf"
+	##echo "cp /usr/share/doc/compton/examples/compton.sample.conf $HOME/.config/openbox/compton/compton.conf"
+
+
+	cp "./config/compton/compton.conf" "$HOME/.config/lxqt/compton/compton.conf"
+	echo "cp ./config/compton/compton.conf $HOME/.config/lxqt/compton/compton.conf"
+
+
+}
+##
+### Tail: compton
+################################################################################
+
+
+################################################################################
+### Head: pcmanfm-qt
+##
+pcmanfm_qt_config_install () {
+	mkdir -p $HOME/.config/pcmanfm-qt/lxqt
+	echo "mkdir -p $HOME/.config/pcmanfm-qt/lxqt"
+
+	cp ./config/pcmanfm-qt/lxqt/settings.conf $HOME/.config/pcmanfm-qt/lxqt/settings.conf
+	echo "cp ./config/pcmanfm-qt/lxqt/settings.conf $HOME/.config/pcmanfm-qt/lxqt/settings.conf"
+
+	return
+
+	mkdir -p "$HOME/.config/pcmanfm-qt/default"
+	echo "mkdir -p $HOME/.config/pcmanfm-qt/default"
+
+	cp ./config/pcmanfm-qt/default/settings.conf "$HOME/.config/pcmanfm-qt/default/settings.conf"
+	echo "cp ./config/pcmanfm-qt/default/settings.conf $HOME/.config/pcmanfm-qt/default/settings.conf"
+
+}
+##
+### Tail: pcmanfm-qt
+################################################################################
+
+
+################################################################################
+### Head: rofi
+##
+rofi_config_install () {
+	mkdir -p "$HOME/.config/rofi"
+	echo "mkdir -p $HOME/.config/rofi"
+
+	cp "./config/rofi/config" "$HOME/.config/rofi/config"
+	echo "cp ./config/rofi/config $HOME/.config/rofi/config"
+
+	rofi_put_app_desktop_entry
+}
+
+rofi_put_app_desktop_entry () {
+	mkdir -p "$HOME/.local/share/applications"
+	echo "mkdir -p $HOME/.local/share/applications"
+
+	cp "./config/rofi/rofi-show-drun.desktop" "$HOME/.local/share/applications/rofi-show-drun.desktop"
+	echo "cp ./config/rofi/rofi-show-drun.desktop $HOME/.local/share/applications/rofi-show-drun.desktop"
+
+	cp "./config/rofi/rofi-show-run.desktop" "$HOME/.local/share/applications/rofi-show-run.desktop"
+	echo "cp ./config/rofi/rofi-show-run.desktop $HOME/.local/share/applications/rofi-show-run.desktop"
+
+	cp "./config/rofi/rofi-show-window.desktop" "$HOME/.local/share/applications/rofi-show-window.desktop"
+	echo "cp ./config/rofi/rofi-show-window.desktop $HOME/.local/share/applications/rofi-show-window.desktop"
+
+}
+##
+### Tail: rofi
+################################################################################
+
+
+################################################################################
+### Head: sakura
+##
+sakura_config_install () {
+	mkdir -p "$HOME/.config/sakura"
+	echo "mkdir -p $HOME/.config/sakura"
+
+	cp "./config/sakura/sakura.conf" "$HOME/.config/sakura/sakura.conf"
+	echo "cp ./config/sakura/sakura.conf $HOME/.config/sakura/sakura.conf"
+
+
+	sakura_put_my_desktop_entry
+}
+
+sakura_put_my_desktop_entry () {
+	mkdir -p "$HOME/.local/share/applications"
+	echo "mkdir -p $HOME/.local/share/applications"
+
+	cp "/usr/share/applications/sakura.desktop" "$HOME/.local/share/applications/sakura.desktop"
+	echo "cp /usr/share/applications/sakura.desktop $HOME/.local/share/applications/sakura.desktop"
+
+	sed -i 's/^Exec=sakura/Exec=sakura -m/g' "$HOME/.local/share/applications/sakura.desktop"
+	echo "sed -i 's/^Exec=sakura/Exec=sakura -m/g' $HOME/.local/share/applications/sakura.desktop"
+}
+##
+### Tail: sakura
+################################################################################
+
+
+################################################################################
+### Head: fcitx
+##
+fcitx_config_install () {
+	mkdir -p $HOME/.config/fcitx
+	echo "mkdir -p $HOME/.config/fcitx"
+
+	cp ./config/fcitx/profile $HOME/.config/fcitx/profile
+	echo "cp ./config/fcitx/profile $HOME/.config/fcitx/profile"
+
+	fcitx_config_install_im_config
+
+}
+
+fcitx_config_install_im_config () {
+	echo
+	im-config -n fcitx
+	echo "im-config -n fcitx"
+	echo "cat ~/.xinputrc"
+	cat ~/.xinputrc
+	echo
+}
+##
+### Tail: fcitx
+################################################################################
+
+
+################################################################################
+### Head: gtk3
+##
+gtk3_config_install () {
+	mkdir -p $HOME/.config/gtk-3.0
+	echo "mkdir -p $HOME/.config/gtk-3.0"
+
+	cp ./config/gtk3/settings.ini $HOME/.config/gtk-3.0/settings.ini
+	echo "cp ./config/gtk3/settings.ini $HOME/.config/gtk-3.0/settings.ini"
+}
+##
+### Tail: gtk3
+################################################################################
+
+
+################################################################################
+### Head: gtk2
+##
+gtk2_config_install () {
+
+	cp ./config/gtk2/.gtkrc-2.0 $HOME/.gtkrc-2.0
+	echo "cp ./config/gtk2/.gtkrc-2.0 $HOME/.gtkrc-2.0"
+
+}
+##
+### Tail: gtk2
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_config_install () {
+	lxqt_config_install
+
+	openbox_config_install
+
+	compton_config_install
+
+	pcmanfm_qt_config_install
+
+	rofi_config_install
+
+	sakura_config_install
+
+	fcitx_config_install
+
+	gtk3_config_install
+
+	gtk2_config_install
+}
+
+## start
+main_config_install
+
+##
+### Tail: main
+################################################################################
