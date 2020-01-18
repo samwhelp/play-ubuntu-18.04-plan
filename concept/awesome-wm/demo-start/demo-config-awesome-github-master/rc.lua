b9c0c6819674f8afac0acc9e7aadd802b41c119a
samwhelp <samwhelp@users.noreply.github.com>
2020-01-19 03:04:10 +0800

    style/experiment

--- concept/awesome-wm/demo-start/demo-config-awesome-github-master/rc.lua
+++ concept/awesome-wm/demo-start/demo-config-awesome-github-master/rc.lua
@@ -3,11 +3,16 @@
 --------------------------------------------------------------------------------
 --- Head: Style
 
+-- awesome themes
 --local rc = "style.awesome_default.rc"
-local rc = "style.awesome_gtk.rc"
+--local rc = "style.awesome_gtk.rc"
 --local rc = "style.awesome_zenburn.rc"
 --local rc = "style.awesome_sky.rc"
 --local rc = "style.awesome_xresources.rc"
+
+
+-- my experiment
+local rc = "style.experiment.rc"
 
 
 --- Tail: Style
