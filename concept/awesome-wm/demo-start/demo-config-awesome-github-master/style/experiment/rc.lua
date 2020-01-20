f624938ac320b46d605383120ea0d6eebef77051
samwhelp <samwhelp@users.noreply.github.com>
2020-01-20 13:52:29 +0800

    layout

--- concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
+++ concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
@@ -91,21 +91,38 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- {{{ Tag
 -- Table of layouts to cover with awful.layout.inc, order matters.
 tag.connect_signal("request::default_layouts", function()
-    awful.layout.append_default_layouts({
-        awful.layout.suit.floating,
-        awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.max,
-        awful.layout.suit.tile,
-        awful.layout.suit.tile.left,
-        awful.layout.suit.tile.bottom,
-        awful.layout.suit.tile.top,
-        awful.layout.suit.fair,
-        awful.layout.suit.fair.horizontal,
-        awful.layout.suit.spiral,
-        -- awful.layout.suit.max.fullscreen,
-        awful.layout.suit.magnifier,
-        awful.layout.suit.corner.nw,
-    })
+	awful.layout.append_default_layouts({
+		awful.layout.suit.tile,
+		awful.layout.suit.tile.bottom,
+		awful.layout.suit.floating,
+		awful.layout.suit.max,
+		-- awful.layout.suit.tile.left,
+		--awful.layout.suit.tile.top,
+		--awful.layout.suit.fair,
+		--awful.layout.suit.fair.horizontal,
+		--awful.layout.suit.spiral,
+		-- awful.layout.suit.spiral.dwindle,
+		-- awful.layout.suit.max.fullscreen,
+		-- awful.layout.suit.magnifier,
+		-- awful.layout.suit.corner.nw,
+	})
+
+	-- awful.layout.append_default_layouts({
+	-- 	awful.layout.suit.floating,
+	-- 	awful.layout.suit.spiral.dwindle,
+	-- 	awful.layout.suit.max,
+	-- 	awful.layout.suit.tile,
+	-- 	awful.layout.suit.tile.left,
+	-- 	awful.layout.suit.tile.bottom,
+	-- 	awful.layout.suit.tile.top,
+	-- 	awful.layout.suit.fair,
+	-- 	awful.layout.suit.fair.horizontal,
+	-- 	awful.layout.suit.spiral,
+	-- 	-- awful.layout.suit.max.fullscreen,
+	-- 	awful.layout.suit.magnifier,
+	-- 	awful.layout.suit.corner.nw,
+	-- })
+
 end)
 -- }}}
 
