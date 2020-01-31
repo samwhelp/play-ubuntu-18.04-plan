74ac46614fbd3826ce88560e130adc3fb021ec70
samwhelp <samwhelp@users.noreply.github.com>
2020-01-31 11:17:54 +0800

    rules

--- concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
+++ concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
@@ -749,59 +749,110 @@ end)
 
 -- }}}
 
--- {{{ Rules
+
+--------------------------------------------------------------------------------
+--- Head: Client Rules
+--
+
 -- Rules to apply to new clients.
 awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { focus = awful.client.focus.filter,
-                     raise = true,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
-    },
+	-- All clients will match this rule.
+	{
+		rule = { },
+		properties = {
+			focus = awful.client.focus.filter,
+			raise = true,
+			screen = awful.screen.preferred,
+			placement = awful.placement.no_overlap+awful.placement.no_offscreen
+		}
+	},
 
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          'DTA',  -- Firefox addon DownThemAll.
-          'copyq',  -- Includes session name in class.
-          'pinentry',
-        },
-        class = {
-          'Arandr',
-          'Blueman-manager',
-          'Gpick',
-          'Kruler',
-          'MessageWin',  -- kalarm.
-          'Sxiv',
-          'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
-          'Wpa_gui',
-          'veromix',
-          'xtightvncviewer'},
+	-- Floating clients.
+	{
+		rule_any = {
+			instance = {
+				'DTA',  -- Firefox addon DownThemAll.
+				'copyq',  -- Includes session name in class.
+				'pinentry',
+			},
+			class = {
+				'Arandr',
+				'Blueman-manager',
+				'Gpick',
+				'Kruler',
+				'MessageWin',  -- kalarm.
+				'Sxiv',
+				'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
+				'Wpa_gui',
+				'veromix',
+				'xtightvncviewer'
+			},
 
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          'Event Tester',  -- xev.
-        },
-        role = {
-          'AlarmWindow',  -- Thunderbird's calendar.
-          'ConfigManager',  -- Thunderbird's about:config.
-          'pop-up',       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
+			-- Note that the name property shown in xprop might be set slightly after creation of the client
+			-- and the name shown there might not match defined rules here.
+			name = {
+				'Event Tester',  -- xev.
+			},
+			role = {
+				'AlarmWindow',  -- Thunderbird's calendar.
+				'ConfigManager',  -- Thunderbird's about:config.
+				'pop-up',	   -- e.g. Google Chrome's (detached) Developer Tools.
+			}
+		},
+		properties = { floating = true }
 
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { 'normal', 'dialog' }
-      }, properties = { titlebars_enabled = true }
-    },
+	},
 
-    -- Set Firefox to always map on the tag named '2' on screen 1.
-    -- { rule = { class = 'Firefox' },
-    --   properties = { screen = 1, tag = '2' } },
+	-- Add titlebars to normal clients and dialogs
+	{
+		rule_any = {
+			type = {
+				'normal',
+				'dialog',
+			}
+		},
+		properties = { titlebars_enabled = true }
+	},
+
+	-- Set Firefox to always map on the tag named '2' on screen 1.
+	-- { rule = { class = 'Firefox' },
+	--   properties = { screen = 1, tag = '2' } },
+
+	-- {
+	-- 	rule = { instance = 'sakura' },
+	-- 	properties = { tag = 'Term' or '1' or 'Free' }
+	-- },
+
+	{
+		rule = { instance = 'atom' },
+		properties = { tag = 'Edit' or '2' or 'Free' }
+	},
+
+	{
+		rule = { instance = 'pcmanfm-qt' },
+		properties = { tag = 'File' or '3' or 'Free' }
+	},
+
+	{
+		rule = { class = 'Firefox' },
+		properties = { tag = 'Web' or '4' or 'Free'}
+	},
+
+	{
+		rule = { instance = 'gimp' },
+		properties = { tag = 'Misc' or '1' or 'Free' }
+	},
+
+	{
+		rule = { instance = 'Xephyr' },
+		properties = { tag = 'Free' or '6' or 'Misc' }
+	},
+
 }
--- }}}
+
+--
+--- Tail: Client Rules
+--------------------------------------------------------------------------------
 
 
 --------------------------------------------------------------------------------
@@ -858,7 +909,6 @@ end)
 --
 --- Tail: Client Titlebar
 --------------------------------------------------------------------------------
-
 
 
 --------------------------------------------------------------------------------
