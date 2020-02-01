c6a4d1de83c96b62ecf74eac204b300afa4114f2
samwhelp <samwhelp@users.noreply.github.com>
2020-02-01 12:43:30 +0800

    refactoring

--- concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
+++ concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
@@ -101,9 +101,8 @@ key_alt = 'Mod1'
 key_shift = 'Shift'
 key_ctrl = 'Control'
 
-
 --
--- Tail: beautiful
+-- Tail: Variable Definitions
 --------------------------------------------------------------------------------
 
 
@@ -275,7 +274,7 @@ screen.connect_signal('request::desktop_decoration', function(s)
 	}
 
 	-- Create a tasklist widget
-	s.mytasklist = awful.widget.tasklist {
+	s.tasklist_main = awful.widget.tasklist {
 		screen  = s,
 		filter  = awful.widget.tasklist.filter.currenttags,
 		buttons = {
@@ -285,14 +284,63 @@ screen.connect_signal('request::desktop_decoration', function(s)
 			awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
 			awful.button({ }, 4, function() awful.client.focus.byidx( 1) end),
 			awful.button({ }, 5, function() awful.client.focus.byidx(-1) end),
-		}
+		},
+
+		-- https://awesomewm.org/apidoc/widgets/awful.widget.tasklist.html
+		style = {
+			border_width = 1,
+			border_color = '#777777',
+			shape = gears.shape.rounded_bar,
+		},
+
+		layout = {
+			spacing = 10,
+			spacing_widget = {
+				{
+					forced_width = 5,
+					shape = gears.shape.circle,
+					widget = wibox.widget.separator
+				},
+				valign = 'center',
+				halign = 'center',
+				widget = wibox.container.place,
+			},
+			layout = wibox.layout.flex.horizontal
+		},
+		-- Notice that there is *NO* wibox.wibox prefix, it is a template,
+		-- not a widget instance.
+		widget_template = {
+			{
+				{
+					{
+						{
+							id = 'icon_role',
+							widget = wibox.widget.imagebox,
+						},
+						margins = 2,
+						widget  = wibox.container.margin,
+					},
+					{
+						id = 'text_role',
+						widget = wibox.widget.textbox,
+					},
+					layout = wibox.layout.fixed.horizontal,
+				},
+				left  = 10,
+				right = 10,
+				widget = wibox.container.margin
+			},
+			id = 'background_role',
+			widget = wibox.container.background,
+		},
+
 	}
 
-	-- Create the wibox
-	s.mywibox = awful.wibar({ position = 'top', screen = s })
+	-- Create the Top Panel
+	s.panel_main = awful.wibar({ position = 'top', screen = s })
 
-	-- Add widgets to the wibox
-	s.mywibox.widget = {
+	-- Add widgets to Top Panel
+	s.panel_main.widget = {
 		layout = wibox.layout.align.horizontal,
 		{ -- Left widgets
 			layout = wibox.layout.fixed.horizontal,
@@ -300,7 +348,7 @@ screen.connect_signal('request::desktop_decoration', function(s)
 			s.mytaglist,
 			s.mypromptbox,
 		},
-		s.mytasklist, -- Middle widget
+		s.tasklist_main, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
 			mykeyboardlayout,
