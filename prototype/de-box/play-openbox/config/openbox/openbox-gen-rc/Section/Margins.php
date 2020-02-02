4d7ecaa1f30559de19bee9f0343a9d9db3bad97a
samwhelp <samwhelp@users.noreply.github.com>
2020-02-03 06:33:29 +0800

    margins

--- prototype/de-box/play-openbox/config/openbox/openbox-gen-rc/Section/Margins.php
+++ prototype/de-box/play-openbox/config/openbox/openbox-gen-rc/Section/Margins.php
@@ -1,10 +1,29 @@
 
+
+<margins>
+	<top>5</top>
+	<bottom>5</bottom>
+	<left>5</left>
+	<right>5</right>
+</margins>
+
+<!--
+<margins>
+	<top>10</top>
+	<bottom>10</bottom>
+	<left>10</left>
+	<right>10</right>
+</margins>
+//-->
+
+<!--
 <margins>
 	<top>0</top>
 	<bottom>0</bottom>
 	<left>0</left>
 	<right>0</right>
 </margins>
+//-->
 
 <?php
 /*
