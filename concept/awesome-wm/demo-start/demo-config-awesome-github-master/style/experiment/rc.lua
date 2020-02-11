e4f4dfc38518e7e4325f836d192899fc7f2a6260
samwhelp <samwhelp@users.noreply.github.com>
2020-02-11 21:08:15 +0800

    fix

--- concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
+++ concept/awesome-wm/demo-start/demo-config-awesome-github-master/style/experiment/rc.lua
@@ -932,14 +932,15 @@ awful.keyboard.append_global_keybindings({
 awful.keyboard.append_global_keybindings({
 
 	awful.key(
-		{ key_alt, key_ctrl }, 'j', function () awful.screen.focus_relative( 1) end,
-		{ description = 'Focus the next screen', group = 'Screen'}
+		{ key_alt, key_ctrl }, 'a', function () awful.screen.focus_relative(-1) end,
+		{ description = 'Focus the previous screen', group = 'Screen'}
 	),
 
 	awful.key(
-		{ key_alt, key_ctrl }, 'k', function () awful.screen.focus_relative(-1) end,
-		{ description = 'Focus the previous screen', group = 'Screen'}
+		{ key_alt, key_ctrl }, 's', function () awful.screen.focus_relative( 1) end,
+		{ description = 'Focus the next screen', group = 'Screen'}
 	),
+
 
 })
 
