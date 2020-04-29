eef5edfed8157c8fdeadc321def223c41491356b
samwhelp <samwhelp@users.noreply.github.com>
2020-04-29 09:30:24 +0800

    fix

--- prototype/de-box/play-pekwm/config/wallpaper/wallpaper.sh
+++ prototype/de-box/play-pekwm/config/wallpaper/wallpaper.sh
@@ -2,8 +2,8 @@
 
 wallpaper_default () {
 	#feh --bg-scale "/usr/share/backgrounds/Manhattan_Sunset_by_Giacomo_Ferroni.jpg"
-	#feh --bg-scale "/usr/share/backgrounds/Spices_in_Athens_by_Makis_Chourdakis.jpg"
-	feh --bg-scale "/usr/share/backgrounds/Frozen_sunset_on_the_lake_by_Manuel_Arslanyan.jpg"
+	feh --bg-scale "/usr/share/backgrounds/Spices_in_Athens_by_Makis_Chourdakis.jpg"
+
 	
 }
 
